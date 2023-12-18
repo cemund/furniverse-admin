859ce3e124316d7397a490a9ac66edf50898e380
Cemund Malagar <cemund36@gmail.com>
2023-12-18 18:35:54 +0800

    fix color

--- lib/widgets/editcolorwidget.dart
+++ lib/widgets/editcolorwidget.dart
@@ -457,7 +457,7 @@ class _EditColorWidgetState extends State<EditColorWidget> {
       // 'price': _priceController.text,
       // 'product 3D model': model,
       'price': double.parse(_priceController.text),
-      'stocks': int.parse(_stocksController.text),
+      'stocks': double.parse(_stocksController.text),
     };
 
     // Add the product to Firestore
